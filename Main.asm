3e125464d384b26de096e60149802e42f9cc0b88
Yuri Jordan <yurijrdn@gmail.com>
2016-07-06 10:38:55 -0300

    merge

--- Main.asm
+++ Main.asm
@@ -2,15 +2,14 @@
 .text
 	li $t0,0
 	lui $t1,0x1001
-	#li $t3,255
 loop:
 	beq $t0,2048,fim
-	lw $t5,ptVerdeLinhaPreta($t0) # $t5 recebe a cor do vetor e pinta o pixel
+	lw $t5,casteloAmarelo($t0) # $t5 recebe a string do vetor e pula pra pintar o pixel
 	beq $t5,120,pintarAmarelo
 	beq $t5,65,pintarCinza
-	sw $t5,0($t1)
+	# Não pintar 
+	addi $t0,$t0,4
 	addi $t1,$t1,4
-	addi $t0,$t0,4	
 	j loop	
 pintarAmarelo:
 	li $t5,16766720 # Cor amarela em decimal
